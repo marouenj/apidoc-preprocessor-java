ad53a9b056645419e33dd38170db9f17bd73b490
marouenj <marouen.jilani@gmail.com>
2016-02-24 15:32:30 +0900

    filter classes annotated with @controller and @RestController

--- scanner/src/main/java/apidoc/preprocessor/scanner/SpringScanner.java
+++ scanner/src/main/java/apidoc/preprocessor/scanner/SpringScanner.java
@@ -1,4 +1,20 @@
 package apidoc.preprocessor.scanner;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.stream.Collectors;
+
 public class SpringScanner extends Scanner {
+
+    @Override
+    protected void keepControllersOnly() {
+        classes = classes.stream()
+                .filter(clazz -> {
+                    Controller controller = clazz.getAnnotation(Controller.class);
+                    RestController restController = clazz.getAnnotation(RestController.class);
+                    return controller != null || restController != null;
+                })
+                .collect(Collectors.toSet());
+    }
 }
---
+++ scanner/src/test/java/apidoc/preprocessor/scanner/SpringScannerIT.java
@@ -0,0 +1,67 @@
+package apidoc.preprocessor.scanner;
+
+import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class SpringScannerIT {
+
+    private static Method classes;
+    private static Method keepControllersOnly;
+
+    @BeforeClass
+    public void init() throws NoSuchMethodException {
+        classes = Scanner.class.getDeclaredMethod("classes", String[].class);
+        classes.setAccessible(true);
+
+        keepControllersOnly = SpringScanner.class.getDeclaredMethod("keepControllersOnly");
+        keepControllersOnly.setAccessible(true);
+    }
+
+    @DataProvider(name = "keepControllersOnly")
+    public Object[][] keepControllersOnly() {
+        return new Object[][]{
+                {
+                        new String[]{"apidoc.preprocessor.scanner.sample_a"},
+                        new TreeSet<String>() {{
+                            add("class apidoc.preprocessor.scanner.sample_a.Controller1");
+                            add("class apidoc.preprocessor.scanner.sample_a.Controller2");
+                            add("class apidoc.preprocessor.scanner.sample_a.Controller3");
+                            add("class apidoc.preprocessor.scanner.sample_a.sample2.Controller4");
+                            add("class apidoc.preprocessor.scanner.sample_a.sample2.sample3.Controller5");
+                        }}
+                },
+                {
+                        new String[]{
+                                "apidoc.preprocessor.scanner.sample_a.sample2",
+                        },
+                        new TreeSet<String>() {{
+                            add("class apidoc.preprocessor.scanner.sample_a.sample2.Controller4");
+                            add("class apidoc.preprocessor.scanner.sample_a.sample2.sample3.Controller5");
+                        }}
+                },
+        };
+    }
+
+    @Test(dataProvider = "keepControllersOnly")
+    public void keepControllersOnly(String[] basePackages, Set<String> expected) throws IllegalAccessException, InstantiationException, InvocationTargetException {
+        SpringScanner scanner = new SpringScanner();
+
+        classes.invoke(scanner, (Object) basePackages);
+        keepControllersOnly.invoke(scanner);
+        Set<Class<?>> classes = scanner.classes;
+
+        Assert.assertNotNull(classes);
+        Assert.assertEquals(classes.size(), expected.size());
+
+        for (Class<?> clazz : classes) {
+            Assert.assertTrue(expected.contains(clazz.toString()));
+        }
+    }
+}
