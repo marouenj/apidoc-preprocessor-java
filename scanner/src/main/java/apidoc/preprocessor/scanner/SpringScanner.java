34ed342eaf5e2db40287f254ca74d67c98212ad1
marouenj <marouen.jilani@gmail.com>
2016-02-24 14:37:28 +0900

    this is not inherited anymore

--- scanner/src/main/java/apidoc/preprocessor/scanner/SpringScanner.java
+++ scanner/src/main/java/apidoc/preprocessor/scanner/SpringScanner.java
@@ -1,13 +1,4 @@
 package apidoc.preprocessor.scanner;
 
-import apidoc.preprocessor.model.Endpoint;
-
-import java.util.Set;
-
 public class SpringScanner extends Scanner {
-
-    @Override
-    public Set<Endpoint> endpoints(String[] basePackages) {
-        return null;
-    }
 }
