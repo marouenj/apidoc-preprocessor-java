51074e9bb2a4a617934be3589bf16d72549a0c2c
marouenj <marouen.jilani@gmail.com>
2016-02-25 12:26:36 +0900

    edit test package

--- scanner/src/test/java/apidoc/preprocessor/scanner/sample_a/sample2/Controller4.java
+++ scanner/src/test/java/apidoc/preprocessor/scanner/sample_a/sample2/Controller4.java
@@ -1,13 +1,14 @@
 package apidoc.preprocessor.scanner.sample_a.sample2;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(value = "controller4/path1")
 public class Controller4 {
 
-    @RequestMapping(value = "method1")
+    @RequestMapping(method = RequestMethod.GET, path = "method1")
     public void method1() {
     }
 
--- scanner/src/test/java/apidoc/preprocessor/scanner/sample_a/sample2/sample3/Controller5.java
+++ scanner/src/test/java/apidoc/preprocessor/scanner/sample_a/sample2/sample3/Controller5.java
@@ -1,17 +1,18 @@
 package apidoc.preprocessor.scanner.sample_a.sample2.sample3;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(value = {"controller5/path1", "controller5/path2"})
 public class Controller5 {
 
-    @RequestMapping(value = "method1")
+    @RequestMapping(method = RequestMethod.GET, value = "method1")
     public void method1() {
     }
 
-    @RequestMapping(value = "method2")
+    @RequestMapping(method = {RequestMethod.GET, RequestMethod.POST}, value = "method2")
     public void method2() {
     }
 }
